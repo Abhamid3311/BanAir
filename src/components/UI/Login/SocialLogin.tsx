aa2ea33dde2cf9740af1f2b46c5660d1aa615969
Hamid <www.abdulhamid3311@gmail.com>
2023-10-18 06:49:39 +0600

    updated URL

--- src/components/UI/Login/SocialLogin.tsx
+++ src/components/UI/Login/SocialLogin.tsx
@@ -9,7 +9,7 @@ export default function SocialLogin() {
     return (
         <div >
             <Button className='bg-[#4D82E5] mb-2 w-full' onClick={() => signIn("google", {
-                callbackUrl: `http://localhost:3000/dashboard`
+                callbackUrl: `https://banair.vercel.app/dashboard`
             })}>
                 <FcGoogle className="mr-2 h-7 w-7 bg-white p-1 rounded-full" />
                 <p className=' font-bold'> Sign in with Google </p>
@@ -17,7 +17,7 @@ export default function SocialLogin() {
 
 
             <Button color="dark" className='w-full' onClick={() => signIn("github", {
-                callbackUrl: `http://localhost:3000/dashboard`
+                callbackUrl: `https://banair.vercel.app/dashboard`
             })}>
                 <AiOutlineGithub className="mr-2 h-7 w-7  " />
                 <p className=' font-bold'> Sign in with GitHub </p>
